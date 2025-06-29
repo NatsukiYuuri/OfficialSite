4cd24dae543e1d15c0d4a29d429e2a43fba49e0c
NatsukiYuuri <ir.live.yuzu@gmail.com>
2025-06-29 18:11:57 +0900

    スクショページの画像表示整備

--- docs/assets/js/screenShot.js
+++ docs/assets/js/screenShot.js
@@ -1,6 +1,11 @@
 "use strict";
 class ScreenShot {
     constructor() {
+        this.currentPage = 0;
+        this.currentTab = "self";
+        this.VIEW_NUM = 10;
+        this.SELF_SS_MAX = 3;
+        this.PRESENT_SS_MAX = 0;
         // タブ切り替え
         const elements = document.querySelectorAll('._tab-item');
         elements.forEach(element => {
@@ -9,8 +14,64 @@ class ScreenShot {
                     elem2.classList.remove('_select');
                 });
                 element.classList.add("_select");
+                this.UpdateTab();
             });
         });
+        this.UpdateTab();
+    }
+    UpdateTab() {
+        // 選択中のタブ
+        const selfTab = document.querySelector("#screen-shot-tab-self");
+        const presentTab = document.querySelector("#screen-shot-tab-present");
+        const isSelectSelf = selfTab?.classList.contains("_select");
+        if (isSelectSelf) {
+            if (this.currentTab != "self") {
+                this.currentTab = "self";
+                this.currentPage = 0;
+            }
+        }
+        else if (presentTab?.classList.contains("_select")) {
+            if (this.currentTab != "present")
+                this.currentTab = "present";
+            this.currentPage = 0;
+        }
+        // タブ変更なし
+        else
+            return;
+        // 現在ある要素の削除
+        const target = document.querySelector("#screen-shot-picture-list");
+        if (target) {
+            while (target.firstChild) {
+                target.removeChild(target.firstChild);
+            }
+        }
+        // 提供タブのとき、共有ボタンを作成
+        if (!isSelectSelf) {
+            const shareBtn = document.createElement("button");
+            shareBtn.textContent = "共有する";
+            shareBtn.classList.add("_share-button");
+            target.appendChild(shareBtn);
+            shareBtn.addEventListener("click", (e) => {
+                const message = "%23名月ゆうり思い出保管庫";
+                window.open("https://x.com/intent/post?text=" + message, '_blank');
+            });
+        }
+        // 現在ページ読み込み
+        for (let i = 0; i < this.VIEW_NUM; i++) {
+            const srcCnt = this.VIEW_NUM * this.currentPage + i;
+            // 画像数を超える
+            if (srcCnt >= (isSelectSelf ? this.SELF_SS_MAX : this.PRESENT_SS_MAX))
+                break;
+            const imgName = srcCnt.toString().padStart(5, "0");
+            const dir = "assets/res/screen_shot/" + (isSelectSelf ? "self/" : "present");
+            // 要素作成して入れる
+            const img = document.createElement("img");
+            img.src = dir + imgName + ".png";
+            const wrapper = document.createElement("div");
+            wrapper.classList.add("_picture-list-elem-wrapper");
+            wrapper.appendChild(img);
+            target.appendChild(wrapper);
+        }
     }
 }
 new ScreenShot();
--- src/ts/screenShot.ts
+++ src/ts/screenShot.ts
@@ -1,4 +1,11 @@
 class ScreenShot {
+    private currentPage = 0;
+    private currentTab: "self"|"present" = "self";
+
+    private readonly VIEW_NUM = 10;
+    private readonly SELF_SS_MAX = 3;
+    private readonly PRESENT_SS_MAX = 0;
+
     constructor() {
         // タブ切り替え
         const elements = document.querySelectorAll<HTMLElement>('._tab-item');
@@ -8,8 +15,72 @@ class ScreenShot {
                     elem2.classList.remove('_select');
                 })
                 element.classList.add("_select");
+                this.UpdateTab()
             })
         });
+
+        this.UpdateTab()
+    }
+
+    private UpdateTab() {
+        // 選択中のタブ
+        const selfTab = document.querySelector("#screen-shot-tab-self") as HTMLElement;
+        const presentTab = document.querySelector("#screen-shot-tab-present") as HTMLElement;
+
+        const isSelectSelf = selfTab?.classList.contains("_select")
+        if(isSelectSelf) {
+            if(this.currentTab != "self") {
+                this.currentTab = "self";
+                this.currentPage = 0;
+            }
+        }
+        else if(presentTab?.classList.contains("_select")) {
+            if(this.currentTab != "present")
+                this.currentTab = "present";
+                this.currentPage = 0;
+        }
+        // タブ変更なし
+        else 
+            return;
+
+
+        // 現在ある要素の削除
+        const target = document.querySelector("#screen-shot-picture-list") as HTMLElement;
+        if (target) {
+            while (target.firstChild) {
+                target.removeChild(target.firstChild);
+            }
+        }
+        // 提供タブのとき、共有ボタンを作成
+        if(!isSelectSelf) {
+            const shareBtn = document.createElement("button");
+            shareBtn.textContent = "共有する"
+            shareBtn.classList.add("_share-button")
+            target.appendChild(shareBtn);
+            shareBtn.addEventListener("click", (e) => {
+                const message = "%23名月ゆうり思い出保管庫"
+                window.open("https://x.com/intent/post?text="+message, '_blank');
+            })
+        }
+
+        // 現在ページ読み込み
+        for(let i=0; i < this.VIEW_NUM; i++) {
+            const srcCnt = this.VIEW_NUM * this.currentPage + i;
+            // 画像数を超える
+            if(srcCnt >= (isSelectSelf?this.SELF_SS_MAX:this.PRESENT_SS_MAX))
+                break;
+
+            const imgName = srcCnt.toString().padStart(5, "0");
+            const dir = "assets/res/screen_shot/" + (isSelectSelf? "self/":"present")
+
+            // 要素作成して入れる
+            const img = document.createElement("img");
+            img.src = dir + imgName + ".png";
+            const wrapper = document.createElement("div");
+            wrapper.classList.add("_picture-list-elem-wrapper")
+            wrapper.appendChild(img)
+            target.appendChild(wrapper)
+        }
     }
 
 }
