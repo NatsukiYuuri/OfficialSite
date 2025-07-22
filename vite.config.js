ddd082a9e9aa3665e9e69e1658a8a9e157a95d5c
NatsukiYuuri <ir.live.yuzu@gmail.com>
2025-07-22 09:29:27 +0900

    パス修正

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './docs',               // index.html がある場所
-  base: '/OfficialSite/',       // 追加：開発・本番で共通のベースパス
-  build: {
-    outDir: './docs/OfficialSite/', // 出力先を指定（相対パス注意）
-    emptyOutDir: true,
+export default {
+  root: './docs', // index.html のある場所
+  css: {
+    preprocessorOptions: {
+      stylus: {},
+    },
   },
-});
+};
