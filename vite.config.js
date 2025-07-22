cf1aae7084be03a7e6359a9c9278cc642528c473
NatsukiYuuri <ir.live.yuzu@gmail.com>
2025-07-22 09:20:55 +0900

    パス修正テスト

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 // vite.config.js
-export default {
-  root: './docs', // index.html のある場所
-  css: {
-    preprocessorOptions: {
-      stylus: {},
-    },
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './docs',               // index.html がある場所
+  base: '/OfficialSite/',       // 追加：開発・本番で共通のベースパス
+  build: {
+    outDir: './docs/OfficialSite/', // 出力先を指定（相対パス注意）
+    emptyOutDir: true,
   },
-};
+});
