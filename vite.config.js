c7400c92e7915047d408266d63be1e789bf74cd6
NatsukiYuuri <ir.live.yuzu@gmail.com>
2025-07-26 01:11:05 +0900

    build setting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
-// vite.config.js
-export default {
+import { defineConfig } from 'vite';
+import path from 'path';
+import glob from 'fast-glob';
+
+// tsファイルをすべて取得
+const inputFiles = Object.fromEntries(
+  glob.sync('src/ts/**/*.ts').map(file => {
+    // src/ts/foo/bar.ts → キーは foo/bar (拡張子なし)
+    const relativePath = path.relative('src/ts', file).replace(/\.ts$/, '');
+    return [relativePath, path.resolve(__dirname, file)];
+  })
+);
+
+export default defineConfig({
   root: './docs', // index.html のある場所
   css: {
     preprocessorOptions: {
       stylus: {},
     },
   },
-};
+  build: {
+    rollupOptions: {
+      input: inputFiles,
+      output: {
+        entryFileNames: 'assets/js/[name].js',
+        format: 'es', // <script>タグで使えるように
+        inlineDynamicImports: false,  // これを追加
+      },
+      external: [], // 依存ライブラリを全て含める
+    },
+    outDir: 'docs',
+    emptyOutDir: true,
+    minify: true,
+    lib: false, // ライブラリモードを使わない
+  }
+});
