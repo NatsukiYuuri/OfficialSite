e2d3c22aa89bd6522e708a930770d214ce74849e
NatsukiYuuri <ir.live.yuzu@gmail.com>
2025-08-02 11:17:57 +0900

    test

--- src/ts/special_events/2025_login.ts
+++ src/ts/special_events/2025_login.ts
@@ -26,18 +26,18 @@ class Event2025LoginStamp {
         const referenceTime = await this.PopSetTimeWindowAsync();
         console.log(referenceTime)
 
-        document.querySelector("#time-reset-button") as HTMLElement;
-        document.addEventListener("click", (e) => {
-            this.MyClearData(this.DATA_KEYS.ReferenceTime)
-        })
-        document.querySelector("#add-first-time") as HTMLElement;
-        document.addEventListener("click", (e) => {
-            const data = this.GetDaysCookie();
-            if(!data.includes(1)) {
-                data.push(1);
-                this.SetDaysCookie(data);
-            }
-        })
+        // document.querySelector("#time-reset-button") as HTMLElement;
+        // document.addEventListener("click", (e) => {
+        //     this.MyClearData(this.DATA_KEYS.ReferenceTime)
+        // })
+        // document.querySelector("#add-first-time") as HTMLElement;
+        // document.addEventListener("click", (e) => {
+        //     const data = this.GetDaysCookie();
+        //     if(!data.includes(1)) {
+        //         data.push(1);
+        //         this.SetDaysCookie(data);
+        //     }
+        // })
 
         const wrapper = document.getElementById('calendar-days');
         if(wrapper) {
